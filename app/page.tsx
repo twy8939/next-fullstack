b3086abe3d9d08894bc10e278bc5fb1bc01517f1
twy8939 <twy8939@naver.com>
2024-07-22 22:40:37 +0900

    feat : Metadata 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Create Next App",
+  description: "Next JS Full Stack App",
+};
 
 export default function Home() {
   return <main>Home</main>;
